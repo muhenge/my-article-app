1f60042a4cfde414a3b4f1cdd9ab5eec8b56dd83
muhenge <mugunga5@gmail.com>
2020-07-15 01:34:17 +0200

    basic setup, styles, gems, upload image

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+  include ArticlesHelper
+  before_action :set_article,  only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to articles_path
+  end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    redirect_to articles_path
+  end
+
+
+end
---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,8 @@
+module ArticlesHelper
+  def article_params
+    params.require(:article).permit(:title, :body, :image)
+  end
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
